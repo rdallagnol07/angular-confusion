0449ea3f4f4c9ff1f8a9bc24730f273573b7f9c6
Rene Dall'Agnol <rdallagnol07@gmail.com>
2018-04-13 14:36:33 -0300

    HTTP Part 2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,6 +17,9 @@ export class HomeComponent implements OnInit {
   dish: Dish;
   promotion: Promotion;
   leader: Leader;
+  dishErrMess:string;
+  promoErrMess:string;
+  leaderErrMess:string;
 
   constructor(private dishservice: DishService,
     private promotionservice: PromotionService,
@@ -24,9 +27,17 @@ export class HomeComponent implements OnInit {
     @Inject('BaseURL') private BaseURL) { }
 
   ngOnInit() {
-    this.dishservice.getFeaturedDish().subscribe(dish => this.dish = dish);
-    this.promotionservice.getFeaturedPromotion().subscribe(promotion => this.promotion = promotion);
-    this.leaderservice.getFeaturedLeader().subscribe(leader => this.leader = leader);
+    this.dishservice.getFeaturedDish()
+      .subscribe(dish => this.dish = dish,
+      errmess => this.dishErrMess = <any>errmess);
+
+    this.promotionservice.getFeaturedPromotion()
+      .subscribe(promotion => this.promotion = promotion,
+      errmess => this.promoErrMess = <any>errmess);
+
+    this.leaderservice.getFeaturedLeader()
+      .subscribe(leader => this.leader = leader,
+      errmess => this.leaderErrMess = <any>errmess);
   }
 
 }
